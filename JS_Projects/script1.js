6bf7106e72192f90187683f6b398e25b0afc33cc
kmart42 <kemartin42@gmail.com>
2022-12-21 19:54:17 -0800

    adding javascript scratch work

---
+++ JS_Projects/script1.js
@@ -0,0 +1,14 @@
+function troubleshooting() {
+  const a = "1";
+  const b = "2";
+
+  // Edit between these lines
+  // =================================
+  const result = +a + +b;
+
+  // =================================
+
+  return result;
+}
+console.log(troubleshooting());
+console.log("%c Green text", "font-color: green; font-size:18px;");
